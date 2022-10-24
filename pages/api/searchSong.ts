7d9ff5046c854d09dab96edf4892dd0e69c2c7dd
AnonymousXC <cvxanonymous007@gmail.com>
2022-10-24 16:05:41 +0530

    Fixed API Internal Error.

--- pages/api/searchSong.ts
+++ pages/api/searchSong.ts
@@ -1,9 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 
 const searchSong = async ( req : NextApiRequest, res: NextApiResponse) => {
-  let query = await fetch(`${process.env.MUSIC_API_URL}/search?query=likhe%20jo`, {method: "GET"})
+  let query = await fetch(`${process.env.NEXT_PUBLIC_MUSIC_API_URL}/search?query=likhe%20jo`, {method: "GET"})
   let data = await query.json() 
   res.status(200).json(data)
 }
