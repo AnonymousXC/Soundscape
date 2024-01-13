794461b8614eb61ad42cb1325faa70830c5bd437
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2024-01-13 13:28:38 +0000

    base

--- pages/api/youtubeVideoSearch.js
+++
@@ -1,8 +0,0 @@
-
-async function fetchYoutubeVideos(req, res) {
-    const searchQuery = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}&type=video&q=${req.body.searchQuery}`)
-    const searchData = (await searchQuery.json()).items
-    res.status(200).json(searchData)
-} 
-
-export default fetchYoutubeVideos
