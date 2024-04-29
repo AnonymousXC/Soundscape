ede26ca705f0fe689cbc8c9a837c3a6e142ec57b
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2024-04-29 19:06:51 +0530

    added top bar in chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,11 +1,12 @@
 import {
     Flex,
 } from '@chakra-ui/react'
+import TopBar from './TopBar';
 
 function Chat() {
     return (
         <Flex position={'relative'} top={0} left={0} width={'100%'} background={'background'} height={'calc(100vh - 6.25rem)'}>
-            Chat
+            <TopBar />
         </Flex>
     )
 }
