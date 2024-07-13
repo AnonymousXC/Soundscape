5f8fcb203c8ae67179ea41c6ac84e36b5abb75f4
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2024-07-13 23:00:59 +0530

    fixed updated errors

--- app/song/[id]/page.tsx
+++ app/song/[id]/page.tsx
@@ -11,26 +11,32 @@ import getPlaylists from "@/database/getUserPlaylists";
 async function SongPage({ params, searchParams }: any) {
 
     const id = params.id
-    const data = (await getSongDetails(id)).data[0]
+    let data;
+    if(id) {
+        const dataDetails = await getSongDetails(id)
+        if(dataDetails.data !== null)
+            data = dataDetails.data[0]
+    }
+    console.log(data)
 
     const PlaylistNames = await (await getPlaylists()).data
 
     return (
         <Flex position={'relative'} top={0} left={0} width={'100%'} maxW={'100%'} background={'background'} height={'100%'} px={'1.25rem'} pt={'1rem'} flexDir={'column'} overflowY={'auto'} overflowX={'hidden'} pb={4} gap={8}>
             <Flex gap={5} alignItems={'center'} flexDirection={['column', 'column', 'column']}>
-                <Img src={data.image[2].link} alt="cover image" width={[270, 270, 300]} height={[270, 270, 300]} rounded={8} />
+                <Img src={data?.image[2].link} alt="cover image" width={[270, 270, 300]} height={[270, 270, 300]} rounded={8} />
                 <Flex flexDirection={'column'} gap={4}>
-                    <Text fontSize={'1.5rem'} fontWeight={'600'}> {data.name} </Text>
+                    <Text fontSize={'1.5rem'} fontWeight={'600'}> {data?.name} </Text>
                     <Flex flexDirection={'column'} gap={2} flex={1}>
-                        <Text> Artist : {data.primaryArtists} </Text>
-                        <Text> Album : {data.album.name} </Text>
-                        <Text> Release Date : {data.releaseDate} </Text>
-                        <Text> Play Count : {data.playCount} </Text>
+                        <Text> Artist : {data?.primaryArtists} </Text>
+                        <Text> Album : {data?.album.name} </Text>
+                        <Text> Release Date : {data?.releaseDate} </Text>
+                        <Text> Play Count : {data?.playCount} </Text>
                     </Flex>
                     <Flex mt={5} alignItems={'center'} gap={5} justifyContent={'center'}>
                         <PlayButton searchParams={searchParams} id={id} />
                         <FavouriteButton />
-                        <DownloadButton link={data.downloadUrl[4].link} />
+                        <DownloadButton link={data?.downloadUrl[4].link} />
                         <ShareButton />
                         <AddToPlaylist id={id} playlist={PlaylistNames} />
                     </Flex>
